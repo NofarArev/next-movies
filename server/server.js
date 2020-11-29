3842f268a0cf8a79862a2a1faa25b0e81719379c
nofar <nofar@DESKTOP-RC5F0FV>
2020-11-29 20:26:55 +0200

    return node port to 3000 and change react port to 3001

--- server/server.js
+++ server/server.js
@@ -16,8 +16,8 @@ app.get('/movies', (req, res) => res.send(movies));
 
 app.get('/movies/:id', (req, res) => res.send(movies.filter(movie => movie.id === req.params.id)));
 
-app.listen(3001, function () {
-    console.log(`app listening on port ${3001}!`);
+app.listen(3000, function () {
+    console.log(`app listening on port ${3000}!`);
 });
 
 module.exports = app;
